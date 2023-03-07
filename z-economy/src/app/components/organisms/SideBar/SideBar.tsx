8350601e83ed8646c8764fc93045e50c5c32b869
Agustin <lyricquinn27@gmail.com>
2023-03-07 18:00:30 -0300

    a

--- z-economy/src/app/components/organisms/SideBar/SideBar.tsx
+++ z-economy/src/app/components/organisms/SideBar/SideBar.tsx
@@ -1,44 +1,51 @@
-import { Button, Button2 } from '../../atoms';
+import { Button } from '../../atoms';
 import styles from './SideBar.module.scss';
 import { IconButton } from '../../atoms/Button/IconButton';
 import { TbLayoutSidebarLeftCollapse } from 'react-icons/all';
 import { SidebarButton } from '../../atoms/Button/SidebarButton';
+import { useState } from 'react';
+import React from 'react';
 
 const handleAddAccountClick = () => {};
 
 export function SideBar() {
+  const [activeButton, setActiveButton] = useState('All Accounts');
+
+  const handleButtonClick = (buttonName: any) => {
+    setActiveButton(buttonName);
+  };
+
   return (
     <nav className={styles.side_bar}>
-      <div className="side-button">
-        <Button2>
-          <span>&#9783;</span>Budget
-        </Button2>
-      </div>
-      <div className="side-button">
-        <Button2>
-          <span>&#10064;</span>Reports
-        </Button2>
-      </div>
-      <div className="side-button">
-        <Button2>
-          <span>&#9878;</span>All Accounts
-        </Button2>
-      </div>
       {/*<div>*/}
       {/*  <LeftSidebarCollapsible></LeftSidebarCollapsible>*/}
       {/*</div>*/}
       <div>
-        <SidebarButton active> Budget </SidebarButton>
-        <SidebarButton active> Banks </SidebarButton>
-        <SidebarButton active> Transfers </SidebarButton>
-        <SidebarButton active> Graph </SidebarButton>
+        <SidebarButton active={activeButton === 'Budget'} onClick={() => handleButtonClick('Budget')}>
+          Budget
+        </SidebarButton>
+        <SidebarButton active={activeButton === 'Reports'} onClick={() => handleButtonClick('Reports')}>
+          Reports
+        </SidebarButton>
+        <SidebarButton
+          active={activeButton === 'All Accounts'}
+          onClick={() => handleButtonClick('All Accounts')}
+        >
+          All Accounts
+        </SidebarButton>
+        <SidebarButton active={activeButton === 'Graph'} onClick={() => handleButtonClick('Graph')}>
+          Graph
+        </SidebarButton>
+        <SidebarButton active={activeButton === 'Quesito'} onClick={() => handleButtonClick('Quesito')}>
+          Quesito
+        </SidebarButton>
       </div>
       <div>
         <Button variant="primary" className="add-btn" onClick={handleAddAccountClick}>
           Add Account
         </Button>
       </div>
-      <IconButton className={styles.z_collapsable_button}>
+      <IconButton className={styles.z_collapsible_button}>
         <TbLayoutSidebarLeftCollapse />
       </IconButton>
     </nav>
