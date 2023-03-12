cbbd0896af4b3e8fe2db2250b804298ec5ee825e
Agustin <lyricquinn27@gmail.com>
2023-03-12 10:43:31 -0300

    updated react-table to v8 table test

--- z-economy/src/app/components/organisms/AllAccountsPage/AllAccountsPage.tsx
+++ z-economy/src/app/components/organisms/AllAccountsPage/AllAccountsPage.tsx
@@ -1,6 +1,61 @@
 import styles from './AllAccountsPage.module.scss'
 import cls from 'classnames';
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from '@tanstack/react-table'
+import { useMemo } from "react";
+
+type BalanceData = {
+    id: number;
+    name: string;
+    balance: number;
+}
+
+const originalData: Array<BalanceData>= [];
+
+for (let index = 0; index < 100; index++) {
+    originalData.push({
+        id: index + 1,
+        name: `Account ${index + 1}`,
+        balance: Math.floor(Math.random() * 10_000)
+    });
+}
 export function AllAccountsPage() {
+
+
+    const data = useMemo(() => originalData, []);
+
+    const columnHelper = createColumnHelper<BalanceData>();
+
+    const columns = [
+        columnHelper.accessor('id', {
+            header: () => 'ID',
+            cell: info => info.renderValue(),
+            footer: info => info.column.id,
+        }),
+        columnHelper.accessor('name', {
+            cell: info => info.getValue(),
+            footer: info => info.column.id,
+        }),
+        columnHelper.accessor('balance', {
+            header: () => 'Balance',
+            cell: info => info.renderValue(),
+            footer: info => info.column.id,
+        })
+    ];
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
+//????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+
+    console.log(data);
     return (
         <div className={cls(styles.all_accounts_page)}>
             <section className={cls('z_flex', styles.all_accounts_page_title)}>
@@ -27,7 +82,53 @@ export function AllAccountsPage() {
                 </div>
             </section>
             <section className={cls('z_flex',styles.all_accounts_table)}>
-                <div>Aca hay tabla</div>
+                <div className={styles.table}>Aca hay tabla
+                    <table>
+                        <thead>
+                        {table.getHeaderGroups().map(headerGroup => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map(header => (
+                                    <th key={header.id}>
+                                        {header.isPlaceholder
+                                            ? undefined
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                        </thead>
+                        <tbody>
+                        {table.getRowModel().rows.map(row => (
+                            <tr key={row.id}>
+                                {row.getVisibleCells().map(cell => (
+                                    <td key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                        </tbody>
+                        <tfoot>
+                        {table.getFooterGroups().map(footerGroup => (
+                            <tr key={footerGroup.id}>
+                                {footerGroup.headers.map(header => (
+                                    <th key={header.id}>
+                                        {header.isPlaceholder
+                                            ? undefined
+                                            : flexRender(
+                                                header.column.columnDef.footer,
+                                                header.getContext()
+                                            )}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                        </tfoot>
+                    </table>
+                </div>
             </section>
         </div>
     );
