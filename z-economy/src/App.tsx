792b92856714031858c528846b39cea8b9164a89
sergio222-dev <molinasergio91@gmail.com>
2023-03-17 07:37:52 -0300

    feature/allAccountsPage

--- z-economy/src/App.tsx
+++ z-economy/src/App.tsx
@@ -1,10 +1,16 @@
-import { Home } from './app/page/Home';
 import { ErrorBoundary } from './app/components/organisms/ErrorBoundary/ErrorBoundary';
+import { MainLayout } from './app/page/MainLayout';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from './app/page/routes';
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <ErrorBoundary>
-      <Home />
+      <MainLayout>
+        <RouterProvider router={router} />
+      </MainLayout>
     </ErrorBoundary>
   );
 }
--- z-economy/src/app/page/Home.tsx
+++ z-economy/src/app/page/Home.tsx
@@ -1,16 +1,10 @@
-import { SideBarView, AllAccountsPage } from '../components/organisms';
+import { AllAccountsPage } from '../components/organisms';
 import styles from '../templates/main_layout.module.scss';
 
 export const Home = () => {
   return (
-    <div className={styles.z_main_layout}>
-      <SideBarView />
-      <div className={styles.z_table}>
-        <AllAccountsPage />
-      </div>
-      {/*<div className={styles.z_table}>*/}
-      {/*  <TransactionTable />*/}
-      {/*</div>*/}
+    <div className={styles.z_table}>
+      <AllAccountsPage />
     </div>
   );
 };
