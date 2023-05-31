9f7df874e860750d6160841401fd8df8e35db066
Agustin <lyricquinn27@gmail.com>
2023-05-31 17:43:11 -0300

    ⚙️: Updated vite-tsconfig-paths

--- z-economy/vite.config.ts
+++ z-economy/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-plugin-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
