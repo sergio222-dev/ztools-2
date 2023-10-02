8178121153eac9c752080be0b4676b6fa8e3f90f
Agustin <lyricquinn27@gmail.com>
2023-10-02 11:30:39 -0300

    🪟: unbypassed private outlet

--- z-economy/src/app/utils/router/PrivateOutlet.tsx
+++ z-economy/src/app/utils/router/PrivateOutlet.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router';
 import { useAuth } from '@utils/useAuth';
 
 export function PrivateOutlet() {
-  const auth = true; //useAuth();
+  const auth = useAuth();
 
   return auth ? <Outlet /> : <Navigate to="/login" />;
 }
