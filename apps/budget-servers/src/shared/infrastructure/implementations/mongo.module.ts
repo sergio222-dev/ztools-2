7248f8d01c6a3f68ebcb56ad9b113aed01247bce
sergio222-dev <molinasergio91@gmail.com>
2023-10-22 10:07:03 -0300

    ⚙️: add fly io configuration

--- apps/budget-servers/src/shared/infrastructure/implementations/mongo.module.ts
+++ apps/budget-servers/src/shared/infrastructure/implementations/mongo.module.ts
@@ -9,13 +9,15 @@ const mongoSchemasModule = MongooseModule.forFeature(budget.schemas);
 const mongoConnectionModule = async () => {
   // await for env loaded
   await ConfigModule.envVariablesLoaded;
-  return MongooseModule.forRoot(
-    `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_SERVER}:${process.env.MONGO_PORT}`,
-    {
-      dbName: process.env.MONGO_INITDB_DATABASE,
-      authSource: process.env.MONGO_AUTH_SOURCE,
-    },
-  );
+  const mongoServer =
+    process.env.MONGO_PROTOCOL === 'mongodb'
+      ? `${process.env.MONGO_SERVER}:${process.env.MONGO_PORT}`
+      : process.env.MONGO_SERVER;
+  const mongoString = `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${mongoServer}`;
+  return MongooseModule.forRoot(mongoString, {
+    dbName: process.env.MONGO_INITDB_DATABASE,
+    authSource: process.env.MONGO_AUTH_SOURCE,
+  });
 };
 
 @Module({})
