cd36ce57b5e6ff903ce8591e0117dc2d592f1b8a
Agustin <lyricquinn27@gmail.com>
2023-10-26 08:58:35 -0300

    🪟: updated pie chart component logic to work with service

--- z-economy/src/app/components/charts/SpendingPieChart.tsx
+++ z-economy/src/app/components/charts/SpendingPieChart.tsx
@@ -1,8 +1,27 @@
-import { useLayoutEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import * as am5 from '@amcharts/amcharts5';
 import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
 import * as am5percent from '@amcharts/amcharts5/percent';
+import { useCategoryHook } from '@core/budget/category/application/adapter/useCategory.hook';
+import { useSignal } from '@preact/signals-react';
+import { CategoryAnalytics } from '@core/budget/category/domain/CategoryAnalytics';
+
 export function SpendingPieChart() {
+  const { getAllCategoryAnalytics } = useCategoryHook(new Date());
+  const analyticsData = useSignal<CategoryAnalytics[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        analyticsData.value = await getAllCategoryAnalytics();
+      } catch (error) {
+        console.error('Error fetching analytics data:', error);
+      }
+    }
+
+    void fetchData();
+  }, []);
+
   useLayoutEffect(() => {
     const root = am5.Root.new('SpendingPieChartDiv');
 
@@ -43,21 +62,13 @@ export function SpendingPieChart() {
       }),
     );
 
-    // Mock data
-    const data = [
-      {
-        categoryName: 'Research',
-        totalOutflow: 500,
-      },
-      {
-        categoryName: 'Marketing',
-        totalOutflow: 1200,
-      },
-      {
-        categoryName: 'Sales',
-        totalOutflow: 850,
-      },
-    ];
+    // set data
+    const data: any[] = analyticsData.value.map(analytics => {
+      return {
+        categoryName: analytics.categoryName,
+        totalOutflow: analytics.totalOutflow,
+      };
+    });
 
     series.data.setAll(data);
     series.ticks.template.set('visible', false);
@@ -65,13 +76,13 @@ export function SpendingPieChart() {
 
     series.slices.template.set(
       'tooltipText',
-      "{categoryName}:\n[bold]${totalOutflow}\n[/]{valuePercentTotal.formatNumber('0.00')}% of Total",
+      "{categoryName}:\n[bold]${totalOutflow.formatNumber('#,###.00')}\n[/]{valuePercentTotal.formatNumber('0.00')}% of Total",
     );
 
     return () => {
       root.dispose();
     };
-  }, []);
+  }, [analyticsData.value]);
 
   return <div id="SpendingPieChartDiv" style={{ width: '100%', height: '500px' }} />;
 }
